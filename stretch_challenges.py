1d0616cbafbff10adcae0fd28a42df97e2305731
Nick Nguyen <nickmn2006@gmail.com>
2024-10-24 14:59:03 -0700

    added more methods

--- stretch_challenges.py
+++ stretch_challenges.py
@@ -4,6 +4,23 @@ class BankAccount:
         self.account_number = account_number
         self.balance = balance
 
+    def deposit(self, amount):
+        self.balance += amount
+        return f'Amount deposited: ${amount:,.2f} new balance: ${self.balance:,.2f}'
+    
+    def withdraw(self, amount):
+        self.balance -= amount
+        if amount > self.balance:
+            return f'Insufficient funds: overdraft fee of $10.\nWithdrawing ${amount:,.2f}...\nNew balance: -${abs(self.balance - 10):,.2f}'
+        return f'Amount withdrawn: ${amount:,.2f} new balance: ${self.balance:,.2f}'
+    
+    def statement(self, number):
+        if number == self.account_number:
+            return f"Account Number: {self.account_number}\nAccount Holder: {self.name}\nCurrent Balance: ${self.balance:,.2f}"
+        return "Invalid account number"
+    
+    def get_balance(self):
+        return f'Current balance: ${self.balance:,.2f}'
 
     # 1.2% annaul interest rate
     def add_interest(self, years):
@@ -20,7 +37,10 @@ def create_account(name, account_number, balance):
 
 acc1 = create_account('Mitchell', "03141592", 1000)
 print(acc1)
+print(acc1.statement("03141592"))
 print(acc1.add_interest(5))
+print(acc1.withdraw(50))
+print(acc1.deposit(500))
 '''
 Mitchell = BankAccount('Mitchell', "03141592", 1000)
 Dani = BankAccount('Daniel', "987654321", 5000)
