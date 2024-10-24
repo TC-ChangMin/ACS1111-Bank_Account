792a9718ab36b56210d7356b3dd4f0ffda36e6f2
Nick Nguyen <nickmn2006@gmail.com>
2024-10-24 14:55:43 -0700

    created function for making an account

--- stretch_challenges.py
+++ stretch_challenges.py
@@ -4,13 +4,33 @@ class BankAccount:
         self.account_number = account_number
         self.balance = balance
 
-    def checking(self):
-        pass
 
     # 1.2% annaul interest rate
-    def savings(self, balance, years):
-        return f"You will have ${balance * 1.012 * years:,.2f} after {years} years"
+    def add_interest(self, years):
+        return f"You will have ${self.balance * (1+ 0.012 * years):,.2f} after {years} years"
     
     # 1% monthly interest rate
-    def savings2(self, balance, years):
-        return f"You will have ${balance * (1+ (0.01/12)) ** (12 * years):,.2f} after {years} years"
+    def add_compounded_interest(self,years):
+        return f"Your compounded interest will be ${self.balance * (1+ (0.01/12)) ** (12 * years):,.2f} after {years} years"
+    
+
+def create_account(name, account_number, balance):
+    account = BankAccount(name, account_number, balance)
+    return account
+
+acc1 = create_account('Mitchell', "03141592", 1000)
+print(acc1)
+print(acc1.add_interest(5))
+'''
+Mitchell = BankAccount('Mitchell', "03141592", 1000)
+Dani = BankAccount('Daniel', "987654321", 5000)
+Braus = BankAccount('Braus', "123456789", 1000)
+
+bank = [Mitchell, Dani, Braus]
+
+for account in bank:
+    print(account.add_interest(5))
+    print(account.add_compounded_interest(5))
+
+# print(Mitchell.add_interest(4))
+'''
